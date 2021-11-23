e8d8087c9b58455307628a703280b6361a35f5d1
lucasGFlores <82884204+lucasGFlores@users.noreply.github.com>
2021-11-23 09:14:57 -0300

    Add files via upload

---
+++ lógica_computacional/PetShop/src/petshop/Passaro.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package petshop;
+
+/**
+ *
+ * @author lucas_g_flores
+ */
+public class Passaro {
+    String estilo_do_bico;
+    boolean carnivoro;
+
+    public String getEstilo_do_bico() {
+        return estilo_do_bico;
+    }
+
+    public void setEstilo_do_bico(String estilo_do_bico) {
+        this.estilo_do_bico = estilo_do_bico;
+    }
+
+    public boolean isCarnivoro() {
+        return carnivoro;
+    }
+
+    public void setCarnivoro(boolean carnivoro) {
+        this.carnivoro = carnivoro;
+    }
+    
+}
---
+++ lógica_computacional/PetShop/src/petshop/Repteis.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package petshop;
+
+/**
+ *
+ * @author lucas_g_flores
+ */
+public class Repteis {
+    int altura;
+    int largura;
+    boolean vive_em_baixo_da_agua;
+}
